2d6989a10749be84ee9f835fdf5cd3c3966c3811
perkles <andyrodrigo2@gmail.com>
2022-07-06 00:27:29 -0300

    services

---
+++ src/app/components/bookstore-app/product-list/Model/Book.ts
@@ -0,0 +1,8 @@
+export class book{
+    id: string = '';
+    name: string = '';
+    price: number = 0;
+    quantity: number = 0;
+    category: string = '';
+    img: string = '';
+}
---
+++ src/app/components/bookstore-app/product-list/product-list.component.srvice.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { book } from "./Model/Book";
+
+@Injectable()
+
+export class bookService{
+
+    private url = "url"
+
+    httpOptions= {
+        Headers: new HttpHeaders({ 'content-type': 'application/json' })
+    }
+
+    constructor(private http: HttpClient){
+
+    }
+
+    getBook(){
+        return this.http.get(this.url)
+    }
+
+}
